da5756440fdb4d3b0c178dc2c405a1ab58b1f04e
Yonata215 <un.yonatan1@gmail.com>
2023-02-16 00:24:54 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,14 @@
-#!/bin/bash 
-gcc 5-printf  -o  5-printf.c
+#include <stdio.h>
+
+/**
+ *  * main - Prints out strings
+ *   * printf is a function that prints out strings to
+ *    * stdout
+ *     * Return: All good
+ *      */
+
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
