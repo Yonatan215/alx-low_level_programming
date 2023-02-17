a4058a37980a87032f7855b5633c646a57e7cff8
Yonata215 <un.yonatan1@gmail.com>
2023-02-17 15:14:10 -0800

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -26,14 +26,13 @@ int main(void)
 				putchar(32);
 				putchar(c + '0');
 				putchar(d + '0');
-			if (!(a == 9 && b == 8)
+
+				if (!(a == 9 && b == 8))
 				{
 					putchar(44);
 					putchar(32);
 				}
 			}
-		}
-	}
-	putchar(10);
-	return (0);
+		}														}	putchar(10);
+		return (0);
 }
