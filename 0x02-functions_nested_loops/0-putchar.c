00b2697dab80a448918d741a30421737b1854890
Yonatan215 <122811807+Yonatan215@users.noreply.github.com>
2023-02-21 14:52:02 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,12 +5,14 @@
  */
 int main(void)
 {
-	char *s = "_putchar";
+	char *s = "_putchar"; /*arrays of characters or strings*/
+	int i = 0;
 
-	for (int i = 0; i < 8; i++)
-    {
-	    _putchar(s[i]);
-    }
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
 	_putchar('\n');
 
 	return (0);
