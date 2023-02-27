f55e11c265ad233dcd6543d8694792f64d04b9b1
Yonatan215 <122811807+Yonatan215@users.noreply.github.com>
2023-02-27 23:36:45 +0300

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,26 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * puts2 - Prints every other character of a string
-  * @str: The string to be treated
-  *
-  * Return: void
-  */
+ * puts2 - print one char out of 2 of a string
+ * @str: char array string type
+ * Description: Print 1st char, then 3rd, then 5th, etc..
+ */
+
 void puts2(char *str)
 {
 	int i;
-	int j = 0;
 
-	while (str[j] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		j++;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
-
-	for (i = 0; i < j; i += 2)
-	{
-		_putchar(str[i]);
-	}
-
 	_putchar('\n');
 }
-
